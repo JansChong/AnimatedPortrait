5484c009a5c7d16a908e1a320821ed1e13c7a6f9
JansChong <houanz@163.com>
2023-06-09 01:06:36 +0800

    JC style 将key抽成变量并注明key的出处

--- api/requestAPI.js
+++ api/requestAPI.js
@@ -2,16 +2,16 @@
 * @Author: JansChong
 * @Date: 2023-06-04 18:18:32
 * @LastEditors: JansChong
-* @LastEditTime: 2023-06-08 23:54:49
+* @LastEditTime: 2023-06-09 01:03:10
 * @FilePath: /api/requestAPI.js
 * @Description: In User Settings Edit
 */
 
-// 获取人脸识别key:
+// 获取人脸识别key: 百度智能云 - 产品 - 人脸与人体  https://cloud.baidu.com/product/face
 let client_id = "F6kIdvWs1iUneYlHWm2dyxLN";
 let client_secret = "Dv1BqjCYqVTXAkFYmx3ptCCoTIm0jWiR";
 
-// 获取人像动漫化token
+// 获取人像动漫化token: 百度智能云 - 产品 - 图像技术 - 图像特效 - 人像动漫化  https://cloud.baidu.com/product/imageprocess/selfie_anime
 let client_id_anime = "2GBIMbINRigGZDY69sOpIQnB";
 let client_secret_anime = "B1rUDX2GbSKSinhNXzLAuvvEjKhxcjsW";
 
